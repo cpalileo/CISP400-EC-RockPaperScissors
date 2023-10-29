4618d76e5ae997e48c574c89b5a494bb1337ada4
Christopher Palileo <cpalileo@gmail.com>
2023-10-29 14:05:30 -0700

    title screen

---
+++ ec1.cpp
@@ -0,0 +1,27 @@
+/*******************************************************
+# Author:           Christopher Palileo
+# Professor:        Caleb Fowler
+# Class:            CISP 400
+# Assignment:       Extra Credit 1: Rock Paper Scissors
+# Semester & Date:  Spring 2023: 10/27/2023
+# Description:      
+#              
+#******************************************************/
+
+#include<iostream>
+using namespace std;
+
+
+
+main() {
+    titleScreen();
+};
+
+void titleScreen() {
+    cout << "   ___                  _                 ___ _                 _    " << endl;
+    cout << "  / _ \\__ _  ___   /\\_/(_)_ __   __ _    / __\\ |__   ___   ___ | |__ " << endl;
+    cout << " / /_)/ _` |/ _ \\  \\_ _/ | '_ \\ / _` |  / /  | '_ \\ / _ \\ / _ \\| '_ \\" << endl; 
+    cout << "/ ___/ (_| | (_) |  / \\| | | | | (_| | / /___| | | | (_) | (_) | |_) |" << endl;
+    cout << "\\/    \\__,_|\\___/   \\_/|_|_| |_|\\__, | \\____/|_| |_|\\___/ \\___/|_.__/ " << endl;
+    cout << "                                |___/                                 " << endl;
+}
